e9a7c7a84eaf4fc06a126d1c92eeeef7afb6808a
jveiiga <jeferson.veiiga@icloud.com>
2023-05-30 19:23:11 -0300

    adding project

---
+++ src/main/java/com/example/T2Sdemo/T2SdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.T2Sdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SdemoApplication.class, args);
+	}
+
+}
